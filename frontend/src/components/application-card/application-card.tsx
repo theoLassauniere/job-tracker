6f0459dac48fc470937b766aba68fde58998931c
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2026-04-29 16:35:02 +0200

    wip: main component

---
+++ frontend/src/components/application-card/application-card.tsx
@@ -0,0 +1,16 @@
+function ApplicationCard() {
+    return (
+        <div style={{
+            background: 'white',
+            borderRadius: '16px',
+            boxShadow: '0 4px 12px rgba(0,0,0,0.1)',
+            padding: '24px',
+            border: '1px solid #e2e8f0',
+            width: '320px'
+        }}>
+            Je suis une candidature
+        </div>
+    );
+}
+
+export default ApplicationCard;
